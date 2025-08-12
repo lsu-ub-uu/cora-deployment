0af0de79e990a91c656af8d47b4f720c38f56d45
perebartrolisimo <pere.bartroli@ub.uu.se>
2025-08-12 09:50:24 +0000

    move systemUrl to environment specific values files

--- startLocalSystemone.sh
+++ startLocalSystemone.sh
@@ -7,4 +7,4 @@ helm dependency update systemone/
 kubectl create namespace systemone
 kubectl apply -f systemone-secret.yaml --namespace=systemone
 kubectl apply -f systemone-minikube-persistent-volumes.yaml
-helm install my20250528systemone systemone --namespace systemone --set deploy.fitnesse=true
+helm install my20250528systemone systemone --namespace systemone -f systemone-local-values.yaml
